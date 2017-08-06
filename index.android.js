144fe050105f0d0e039a0a9b72b911352eda76bb
Kevin GATERA <kgate066@uottawa.ca>
2017-08-05 20:32:36 -0400

    Added 'import { Component } from 'react-native';'
    This will solve the 'can't find variable Component error' which resulted from not declaring it in the imports of the App component declaration.

--- index.android.js
+++ index.android.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
+import { Component } from 'react-native';
 import App from './app/index';
 
 AppRegistry.registerComponent('FirstLookReactNavigation', () => App);
